00fed06fe7442483516808b9366c40a3075f75a3
rockett366 <91300613+rockett366@users.noreply.github.com>
2024-02-15 21:02:35 -0800

    React setup
    The basic base for react is set up and ready.
    Once NodeJS is downloaded, type in terminal "npm install" then "npm start" to display

---
+++ src/components/pages/Home.js
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+    <div className="App">
+      <header className="Header center">
+        <h1>CSC 130 Class Project</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
