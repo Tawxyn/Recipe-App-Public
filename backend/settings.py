9cfcb647ad06e1dd14c3658287c0d031ceb1058b
Tawxyn <oleshchukmike@gmail.com>
2024-02-28 14:09:01 -0800

    Updated backend foulder
    Created new foulders to house pipelines for mongodb and credentials, fixed .env.cred to just .env to fix enivorment call bug

---
+++ backend/settings.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve the value of the 'MONGO_URI' environment variable
+MONGO_URI = os.environ.get('MONGO_URI')
+
+
+
