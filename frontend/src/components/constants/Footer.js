ee500be6d0320d88a5fd0a17f757e5be4e953466
Rockett <eemejia366@gmail.com>
2024-03-26 15:10:47 -0700

    CSS change to home page
    gave it more space so footer wasnt close to title
    also gave space to give an about
    
    above the about section
    Search (is link to search page) | Cook | Save (link to history page) | Enjoy

--- frontend/src/components/constants/Footer.js
+++ frontend/src/components/constants/Footer.js
@@ -6,6 +6,7 @@ function Footer() {
     <div className="Footer">
       <h3>Ctrl-alt-defeat</h3>
       <div>
+        
         <p>Copyright © 2024 Ctrl-Alt-Defeat. All Rights Reserved.</p>
       </div>
     </div>
